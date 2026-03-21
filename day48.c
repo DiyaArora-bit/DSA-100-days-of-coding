b6306382fe511918f465f9e72dfcd98c7c20a9d2
DiyaArora-bit <diya71712@gmail.com>
2026-03-21 14:58:32 +0530

    Add files via upload

---
+++ day48.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+int countLeafNodes(struct Node* root) {
+    if (root == NULL)
+        return 0;
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+    return countLeafNodes(root->left) + countLeafNodes(root->right);
+}
+
+struct Node* buildTree(int arr[], int i, int n) {
+    if (i >= n || arr[i] == -1)
+        return NULL;
+
+    struct Node* root = createNode(arr[i]);
+    root->left = buildTree(arr, 2 * i + 1, n);
+    root->right = buildTree(arr, 2 * i + 2, n);
+    return root;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, 0, n);
+
+    printf("%d\n", countLeafNodes(root));
+
+    return 0;
+}
